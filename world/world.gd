98af7d21c42e529fba057dbf12bc1cc376e5a748
RM35 <92856544+RM35@users.noreply.github.com>
2022-06-23 22:54:04 +0100

    feat: load all unit data in unit_data directory on world _ready

--- world/world.gd
+++ world/world.gd
@@ -9,11 +9,8 @@ var move_delay = 0.5
 
 #Cards
 onready var unit_scene = preload("res://unit/unit.tscn")
-var card_types: Array = ["res://unit/unit_data/unit_lungs.tres",
-						"res://unit/unit_data/unit_shield.tres",
-						"res://unit/unit_data/unit_skull.tres",
-						"res://unit/unit_data/unit_chicken.tres",
-                        "res://unit/unit_data/unit_friendly_cat.tres"]
+var unit_data_path = "res://unit/unit_data/"
+var card_types: Array = []
 
 #Decks
 onready var enemy_deck = $VB/EnemyCards
@@ -41,6 +38,15 @@ func _ready():
 	for child in shop_deck.get_children():
 		if child.get_filename() == "res://card_slot/card_slot.tscn":
 			shop_slots.append(child)
+	var dir = Directory.new()
+	if dir.open(unit_data_path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir():
+				card_types.append(unit_data_path + file_name)
+			file_name = dir.get_next()
+	print(card_types)
 
 func _process(delta):
 	$Debug/Panel/MarginContainer/VB/Level.text = "GAME LEVEL: " + str(game_level)
